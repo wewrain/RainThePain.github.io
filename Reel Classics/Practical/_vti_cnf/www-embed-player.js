c50ad362ad1667255bc5a90bcd1c6e74eed819c1
wewrain <39387490+wewrain@users.noreply.github.com>
2018-06-19 04:41:10 +0100

    First website upload

---
+++ Reel Classics/Practical/_vti_cnf/www-embed-player.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|20 Mar 2018 06:14:08 -0000
+vti_author:SR|DESKTOP-UH9T861\\R
+vti_modifiedby:SR|DESKTOP-UH9T861\\R
+vti_nexttolasttimemodified:TW|20 Mar 2018 06:14:08 -0000
+vti_timecreated:TR|20 Mar 2018 06:18:42 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Reel\\ Classics/Practical/0HEiqAsrVMQ.htm
+vti_cacheddtm:TX|20 Mar 2018 05:18:44 -0000
+vti_filesize:IR|95120
---
+++ Reel Classics/Practical/videos/_vti_cnf/base.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|12 Jun 2018 14:23:41 -0000
+vti_author:SR|ACADEMIC\\BMC161703999
+vti_modifiedby:SR|ACADEMIC\\BMC161703999
+vti_nexttolasttimemodified:TW|12 Jun 2018 14:23:41 -0000
+vti_timecreated:TR|12 Jun 2018 14:38:09 -0000
+vti_cacheddtm:TX|12 Jun 2018 14:38:10 -0000
+vti_filesize:IR|1287216
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Reel\\ Classics/Practical/videos/0HEiqAsrVMQ.htm
